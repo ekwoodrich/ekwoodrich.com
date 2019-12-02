37e57e5cacb2991b6c431fc13772118c9ef21116
Eliot Kent Woodrich <ekwoodrich@gmail.com>
2019-12-01 21:53:29 -0800

    Restyled landing page.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="">
       <h1>Eliot Woodrich</h1>
-      <h3>B.S. Computer Science at Portland State University</h3>
+      <h5>B.S. Computer Science at Portland State University</h5>
       <h5>(expected graduation: June 2020)</h5>
-      <a href="https://github.com/ekwoodrich">Github</a> <br />
+      <a href="https://github.com/ekwoodrich">Github</a> &#183;&nbsp;
       <a href="https://www.linkedin.com/in/eliot-woodrich-a83459bb">LinkedIn</a>
-      <br />
+      &nbsp;&#183;&nbsp;
       <a href="mailto:eliot@woodrich.com">Email</a>
     </div>
   );
