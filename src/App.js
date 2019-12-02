2d190b8af6ea65800793d5727ea42bb14e72f153
Eliot Kent Woodrich <ekwoodrich@gmail.com>
2019-12-01 19:33:23 -0800

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Eliot Woodrich</h1>
+      <h3>B.S. Computer Science at Portland State University</h3>
+      <h5>(expected graduation: June 2020)</h5>
+      <a href="https://github.com/ekwoodrich">Github</a> <br />
+      <a href="https://www.linkedin.com/in/eliot-woodrich-a83459bb">LinkedIn</a>
+      <br />
+      <a href="mailto:eliot@woodrich.com">Email</a>
+    </div>
+  );
+}
+
+export default App;
