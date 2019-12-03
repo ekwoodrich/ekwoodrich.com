37e4739c1a22c15b57d8e10524dc71ea2ed51b98
Eliot Kent Woodrich <ekwoodrich@gmail.com>
2019-12-02 19:16:45 -0800

    Began adding portfolio elements.

--- src/App.js
+++ src/App.js
@@ -7,14 +7,21 @@ function App() {
       <h1>Eliot Woodrich</h1>
       <h5>B.S. Computer Science at Portland State University</h5>
       <h5>(expected graduation: June 2020)</h5>
-      <a href="https://storage.googleapis.com/ekwoodrich/EliotWoodrichLinkedIn.pdf">
-        Resume/CV
-      </a>{' '}
-      &#183;&nbsp;
-      <a href="https://github.com/ekwoodrich">Github</a> &#183;&nbsp;
-      <a href="https://www.linkedin.com/in/eliot-woodrich-a83459bb">LinkedIn</a>
-      &nbsp;&#183;&nbsp;
-      <a href="mailto:eliot@woodrich.com">Email</a>
+      <div>
+        <a href="https://storage.googleapis.com/ekwoodrich/EliotWoodrichLinkedIn.pdf">
+          Resume/CV
+        </a>{' '}
+        &#183;&nbsp;
+        <a href="/portfolio">Projects</a>
+      </div>
+      <div class="linkItem">
+        <a href="https://github.com/ekwoodrich">Github</a> &#183;&nbsp;
+        <a href="https://www.linkedin.com/in/eliot-woodrich-a83459bb">
+          LinkedIn
+        </a>
+        &nbsp;&#183;&nbsp;
+        <a href="mailto:eliot@woodrich.com">Email</a>
+      </div>
     </div>
   );
 }
