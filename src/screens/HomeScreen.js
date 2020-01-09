c1b3e2a5c193d0589ed8cdfe4786e56f526733b2
Eliot Kent Woodrich <ekwoodrich@gmail.com>
2020-01-08 19:15:15 -0800

    Added footer to link to source code.

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -18,31 +18,35 @@ import {
   useParams
 } from 'react-router-dom';
 import ProjectsScreen from './ProjectsScreen';
+import Footer from '../components/Footer';
 
 function HomeScreen(props) {
   return (
-    <div className="">
-      <h1>Eliot Woodrich</h1>
-      <h5 id="schoolInfo">Full Stack Developer</h5>
-      {/* <div class="buttonLine">
+    <div>
+      <div className="">
+        <h1>Eliot Woodrich</h1>
+        <h5 id="schoolInfo">Full Stack Developer</h5>
+        {/* <div class="buttonLine">
         <LinkButton url="https://storage.googleapis.com/ekwoodrich/EliotWoodrichLinkedIn.pdf">
           <FaGithub /> Resume
         </LinkButton>
       </div> */}
-      <div class="buttonLine">
-        <div class="linkItem">
-          <LinkButton url="https://github.com/ekwoodrich">
-            <FaGithub /> Github
-          </LinkButton>
-          <LinkButton url="https://www.linkedin.com/in/eliot-woodrich-a83459bb">
-            <FaLinkedin /> LinkedIn
-          </LinkButton>
-          <LinkButton url="mailto:ekwoodrich@gmail.com">
-            <FaEnvelope /> Email{' '}
-          </LinkButton>
+        <div class="buttonLine">
+          <div class="linkItem">
+            <LinkButton url="https://github.com/ekwoodrich">
+              <FaGithub /> Github
+            </LinkButton>
+            <LinkButton url="https://www.linkedin.com/in/eliot-woodrich-a83459bb">
+              <FaLinkedin /> LinkedIn
+            </LinkButton>
+            <LinkButton url="mailto:ekwoodrich@gmail.com">
+              <FaEnvelope /> Email{' '}
+            </LinkButton>
+          </div>
         </div>
+        <ProjectsScreen />
       </div>
-      <ProjectsScreen />
+      <Footer />
     </div>
   );
 }
